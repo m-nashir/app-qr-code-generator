6f33ff014e674f31a6f57f9efa15d64049ad85c4
m-nashir <muh.nashir124@gmail.com>
2023-01-11 06:32:17 +0700

    #done app-qr-code-generator

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const wrapper = document.querySelector('.wrapper'),
 qrInput = document.querySelector('.form input')
-generateBtn = document.querySelector('.form button');
+generateBtn = document.querySelector('.form button'),
+qrImg = document.querySelector('.qr-code img');
 
 generateBtn.addEventListener('click', () => {
     let qrValue = qrInput.value;
-    wrapper.classList.add('active');
+    if(!qrValue) return;
+    generateBtn.innerText = 'Generating QR Code...'
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+    qrImg.addEventListener('load', () => {
+        wrapper.classList.add('active');
+        generateBtn.innerText = 'Generate QR Code';
+    })
+})
+
+qrInput.addEventListener('keyup', () => {
+    if(!qrInput.value);
+    wrapper.classList.remove('active');
 })
